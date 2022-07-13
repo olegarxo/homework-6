728d96d8f3f1e0fe099b5fa8cf8ac7486db2fe11
oleg <olegarxo15@gmail.com>
2022-07-13 14:19:15 +0300

    Сделал 3 дз

---
+++ homework2/Program.cs
@@ -0,0 +1,38 @@
+﻿void intersectionPoint(double[] sumMassive,double b1,double b2,double k1,double k2)
+{
+    //y = k1 * x + b1
+    //y = k2 * x + b2
+    //((k1 - k2)*x) == -(b1 - b2);
+    int length = sumMassive.Length;
+    double x=(b2-b1)/(k1-k2);
+    double y = (k2*x)+b2;
+    sumMassive[0] = x;
+    sumMassive[1] = y;
+}
+void printMassive(double[] sumMassive)
+{
+    int index = 0;
+    int length = sumMassive.Length;
+    while(index<length)
+    {
+        Console.Write($"{sumMassive[index]}\t ");
+        index++;
+    }
+}
+double b1 = 0;
+double b2 = 0;
+double k1 = 0;
+double k2 = 0;
+double[] sumMassive = new double[2];
+Console.WriteLine("Даны отрезки y = k1 * x + b1, y = k2 * x + b2");
+Console.WriteLine("Введите b1");
+b1= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите b2");
+b2= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите k1");
+k1= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите k2");
+k2= int.Parse(Console.ReadLine());
+intersectionPoint(sumMassive,b1,b2,k1,k2);
+printMassive(sumMassive);
+
